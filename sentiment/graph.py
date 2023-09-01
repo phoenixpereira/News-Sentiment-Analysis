336cdbb0f18ced82160e012194bbe59eb4654ada
Phoenix Isaac Pereira <phoenixpereira99@gmail.com>
2023-09-01 11:25:58 +0930

    Add sentiment graph

--- sentiment/graph.py
+++ sentiment/graph.py
@@ -16,8 +16,7 @@ sentiment_directory = 'sentiment/results'
 sentiments_data = []
 
 for country_code in country_mapping.keys():
-    file_path = os.path.join(
-        sentiment_directory, f'sentiments_{country_code}.csv')
+    file_path = os.path.join(sentiment_directory, f'{country_code}.csv')
     data = pd.read_csv(file_path)
     data['Country'] = country_mapping[country_code]
     sentiments_data.append(data)
@@ -28,23 +27,39 @@ combined_data = pd.concat(sentiments_data, ignore_index=True)
 # Convert publish_date to datetime
 combined_data['publish_date'] = pd.to_datetime(combined_data['publish_date'])
 
-# Plotting the combined trend lines
-plt.figure(figsize=(12, 8))
+# Calculate the total count of headlines for each country and interval
+combined_data['Total'] = combined_data['Positive'] + combined_data['Negative']
+
+# Calculate the percentage of positive headlines for each country and interval
+combined_data['Positive Percentage'] = (
+    combined_data['Positive'] / combined_data['Total']) * 100
+
+# Calculate y-axis values based on sentiment percentage
+combined_data['Sentiment'] = combined_data['Positive Percentage'].apply(
+    lambda x: x - 100 if x <= 50 else x - 50)
+
+# Plotting the combined graph
+fig, ax = plt.subplots(figsize=(12, 8))
 
 for country_name, country_data in combined_data.groupby('Country'):
-    plt.plot(country_data['publish_date'],
-             country_data['Positive'], label=f'{country_name} (Positive)')
-    plt.plot(country_data['publish_date'],
-             country_data['Negative'], label=f'{country_name} (Negative)')
+    ax.plot(country_data['publish_date'],
+            country_data['Sentiment'], label=country_name)
 
-plt.xlabel('Date')
-plt.ylabel('Count')
-plt.title('News Headline Sentiment Over Time')
-plt.legend()
+ax.set_ylabel('Sentiment Score')
+ax.set_title('News Headline Sentiment Over Time')
+ax.set_ylim(-100, 100)  # Set y-axis limits to -100 to 100
 
 # Set x-axis labels for every year
 years = pd.to_datetime(combined_data['publish_date']).dt.year.unique()
 plt.xticks(ticks=pd.to_datetime(years, format='%Y'), labels=years, rotation=45)
+
+# Add horizontal lines at 0, 50, and -50 for reference
+ax.axhline(y=0, color='black', linestyle='--', linewidth=0.8)
+ax.axhline(y=50, color='gray', linestyle='--', linewidth=0.8)
+ax.axhline(y=-50, color='gray', linestyle='--', linewidth=0.8)
+
+# Add legend
+ax.legend()
 
 plt.tight_layout()
 plt.show()
