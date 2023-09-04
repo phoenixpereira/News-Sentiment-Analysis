57cfaf821d29d72dd819e1041172b69d0b08a5f6
Phoenix Isaac Pereira <phoenixpereira99@gmail.com>
2023-09-04 15:55:47 +0930

    Add keywords results

--- keywords/analysis.py
+++ keywords/analysis.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 from datetime import datetime
 
 # Read the data from the CSV file
-data = pd.read_csv('filteredDatasets/AU2.csv')
+data = pd.read_csv('filteredDatasets/AU.csv')
 df = pd.DataFrame(data, columns=['publish_date', 'headline_text'])
 
 # Extract the publish dates and headlines from the DataFrame
@@ -37,9 +37,6 @@ def extract_keywords(batch_headlines):
 # Determine the time periods (e.g., quarterly intervals)
 start_date = datetime.strptime(min(publish_dates), '%Y-%m-%d')
 end_date = datetime.strptime(max(publish_dates), '%Y-%m-%d')
-quarter_length = 3  # Number of months in a quarter
-current_start_date = start_date
-current_end_date = start_date + pd.DateOffset(months=quarter_length)
 
 # Initialize a list to store keyword data for the CSV file
 csv_data = []
@@ -52,39 +49,30 @@ with tqdm(total=total_headlines, desc="Extracting Keywords", unit="headline", dy
 
         # Check if the headline is within the current time period (quarter)
         publish_date = datetime.strptime(batch_date, '%Y-%m-%d')
-        if publish_date >= current_start_date and publish_date < current_end_date:
-            # Extract keywords from the headline and update counts for the current time period
-            keywords_list = extract_keywords([batch_headline])
-            for keyword in keywords_list:
-                keywords[current_start_date][keyword] += 1
 
-        # Move to the next quarter if necessary
-        if publish_date >= current_end_date:
-            current_start_date = current_end_date
-            current_end_date += pd.DateOffset(months=quarter_length)
+        # Determine the quarter for the current headline
+        quarter = f'{publish_date.year}Q{int((publish_date.month - 1) / 3) + 1}'
 
-            # Sort and extract the top 100 keywords for the current time period
-            top_keywords = sorted(keywords[current_start_date], key=keywords[current_start_date].get, reverse=True)[:100]
-            
-            # Add keyword data to the CSV data list
-            for keyword in top_keywords:
-                csv_data.append({
-                    'Interval': f'{current_start_date.strftime("%Y")}Q{current_start_date.quarter}',
-                    'Top Keyword': keyword,
-                    'Count': keywords[current_start_date][keyword]
-                })
+        # Extract keywords from the headline and update counts for the current time period (quarter)
+        keywords_list = extract_keywords([batch_headline])
+        for keyword in keywords_list:
+            keywords[quarter][keyword] += 1
 
         pbar.update(1)
 
-# Sort and extract the top 25 keywords for the last time period
-top_keywords = sorted(keywords[current_start_date], key=keywords[current_start_date].get, reverse=True)[:25]
-# Add keyword data to the CSV data list for the last time period
-for keyword in top_keywords:
-    csv_data.append({
-        'Interval': f'{current_start_date.strftime("%Y")}Q{current_start_date.quarter}',
-        'Top Keyword': keyword,
-        'Count': keywords[current_start_date][keyword]
-    })
+# Create intervals based on the available data
+available_quarters = sorted(keywords.keys())
+for quarter in available_quarters:
+    # Sort and extract the top 50 keywords for the current time period (quarter)
+    top_keywords = sorted(keywords[quarter], key=keywords[quarter].get, reverse=True)[:50]
+    
+    # Add keyword data to the CSV data list
+    for keyword in top_keywords:
+        csv_data.append({
+            'Interval': quarter,
+            'Top Keyword': keyword,
+            'Count': keywords[quarter][keyword]
+        })
 
 # Create a DataFrame from the CSV data
 csv_df = pd.DataFrame(csv_data)
--- keywords/graph.py
+++ keywords/graph.py
@@ -1,20 +1,71 @@
 import pandas as pd
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
 
-# Load the top keywords data from the CSV file
-top_keywords_df = pd.read_csv('keywords/results/AU.csv')
+# Define the countries and their corresponding CSV files
+countries = {
+    'Australia': 'AU.csv',
+    'Ireland': 'IE.csv',
+    'India': 'IN.csv',
+    'United States': 'US.csv',
+    'United Kingdom': 'UK.csv'
+}
 
-# Create a WordCloud
-wordcloud = WordCloud(width=800, height=400,
-                      background_color='white', colormap='viridis', max_words=500)
+# Create a subplot for the word clouds with 2 rows and 3 columns
+fig, axes = plt.subplots(2, 3, figsize=(12, 8))  # Adjust the figure size as needed
 
-# Generate the word cloud using top keywords
-wordcloud.generate_from_frequencies(
-    dict(zip(top_keywords_df['Top Keyword'], top_keywords_df['Count'])))
+# Flatten the 2D array of subplots into a 1D array
+axes = axes.ravel()
 
-# Display the word cloud
-plt.figure(figsize=(10, 6))
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis('off')
+# Center the top two subplots
+fig.subplots_adjust(top=0.85, wspace=0.4)
+
+# Initialize the time interval slider
+ax_slider = plt.axes([0.2, 0.02, 0.6, 0.03])  # Adjust the position and size of the slider
+intervals = list(range(60))  # Modify the number of intervals as needed
+interval_slider = Slider(ax_slider, 'Intervals', 0, len(intervals) - 1, valinit=0, valstep=1)
+
+# Read all CSV data files for the countries
+all_data = {country: pd.read_csv(f'keywords/results/{csv_file}') for country, csv_file in countries.items()}
+
+# Create a title for the top keywords and current interval
+title = fig.suptitle("", fontsize=16)
+
+# Function to update the word clouds based on the selected interval
+def update(val):
+    selected_interval = int(interval_slider.val)
+    for i, (country, data) in enumerate(all_data.items()):
+        # Filter the data for the selected interval
+        filtered_data = data[data['Interval'] == data['Interval'].unique()[selected_interval]]
+
+        # Create a word cloud
+        wordcloud = WordCloud(width=400, height=200, max_words=50, background_color='white').generate_from_frequencies(
+            filtered_data.set_index('Top Keyword')['Count'].to_dict())
+
+        # Update the word cloud on the corresponding subplot
+        axes[i].clear()
+        axes[i].imshow(wordcloud, interpolation='bilinear')
+        axes[i].set_title(country)
+        axes[i].axis('off')
+
+    # Update the title to show the current interval and top keywords
+    current_interval = filtered_data['Interval'].values[0]
+    title.set_text(f"Top 25 Keywords - {current_interval}")
+
+    plt.tight_layout()
+    fig.canvas.draw_idle()
+
+# Attach the update function to the slider
+interval_slider.on_changed(update)
+
+# Initialize the word clouds with the first interval
+update(0)
+
+# Hide any remaining empty subplots
+for i in range(len(countries), len(axes)):
+    axes[i].axis('off')
+
+# Display the plot
+plt.tight_layout()
 plt.show()
